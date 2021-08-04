c2d406b58f8972b9175d7990b197d26fab599186
Jackson Merma <jmermap@unsa.edu.pe>
2021-08-04 14:53:50 -0500

    metodos para implementar una relacion entre datos(lista de adyacencia)

--- Grafo.java
+++ Grafo.java
@@ -20,7 +20,7 @@ public class Grafo<E extends Comparable<E>>{
     private Item bfsList, dfsList;
     
     public Grafo(int n) {
-    	list = (E[]) new Object [n];
+    	list = (E[]) new Object [n];//para recuperar las posiciones
     	listAdya = new int [n][n]; //lista de adyacencia con pesos
     	total=n;
     	
@@ -31,6 +31,48 @@ public class Grafo<E extends Comparable<E>>{
     	for(int i=0;i<total;i++)
     		for(int u=0;u<total;u++)
     			listAdya[i][u]=inf;
+    }
+    
+    public boolean isFull() {
+    	return cant == total;
+    }
+    
+    public void insert(E data) {
+    	if(this.isFull()) {
+    		System.out.println("Esta lleno");
+    	}else {
+    		this.list[cant]= data;
+    		this.cant++;
+    	}
+    }
+    
+    public void relation(E a, E b) {//sin peso
+    	this.relation(a, b, 1);
+    }
+
+    public void relation(E a, E b, int peso) {// con peso
+    	 int posA, posB;
+    	 posA=capturePos(a);
+    	 posB=capturePos(b);
+    	 
+    	 //se coloca la relacion en la lista de Adyacencia
+    	 
+    	 if(posA==-1||posB==-1) {
+    		 System.out.println("Datos incorrectos");
+    	 }else {
+    		 this.listAdya[posA][posB]=peso;
+    		 
+    		 //por defecto tambien se colocara en la contraria
+    		 this.listAdya[posB][posA]=peso;
+    	 }
+    }
+    private int capturePos(E data) {
+    	//se busca en list
+    	for(int i=0;i<this.cant;i++) {
+    		if(list[i].compareTo(data)==0)
+    			return i;
+    	}
+    	return -1;
     }
     
 }
